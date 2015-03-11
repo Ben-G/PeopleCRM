0a3aa09e8462c11b786c4e0dd9b4138e7e12c82c
Ben-G <Benjamin.Encz@gmail.com>
2015-03-10 22:00:17 -0700

    Add functionality to add entry

--- PeopleCRM/ViewController.m
+++ PeopleCRM/ViewController.m
@@ -12,9 +12,11 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *addPersonButton;
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 @property (strong, nonatomic) ArrayCollectionViewDataSource *dataSource;
 @property (strong) NSArray *people;
+@property (strong) RACSignal *editModeSignal;
 
 @end
 
@@ -22,6 +24,10 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+  
+  //TODO: deactivate all other cells when adding / editing a new one
+  
+  @weakify(self)
   // Do any additional setup after loading the view, typically from a nib.
   
   self.dataSource = [[ArrayCollectionViewDataSource alloc] initWithNibFile:@"PersonCollectionViewCell" configureCellBlock:^(id cell, id item) {
@@ -31,7 +37,18 @@
   self.collectionView.dataSource = self.dataSource;
   
   RAC(self.dataSource, items) = [RACObserve(self, people) doNext:^(id x) {
+    @strongify(self);
     [self.collectionView reloadData];
+  }];
+  
+  self.addPersonButton.rac_command = [[RACCommand alloc] initWithEnabled:self.editModeSignal signalBlock:^RACSignal *(id input) {
+    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+      @strongify(self);
+      self.people = [@[@1] arrayByAddingObjectsFromArray:self.people];
+      [subscriber sendCompleted];
+      
+      return nil;
+    }];
   }];
   
   self.people = @[@1, @2, @3, @4];
